04ece7dce3c83345e88c7c18fb06b4ec09f0adba
Kaustubh Dubey <78341839+KAUSTUBHDUBEY790@users.noreply.github.com>
2023-04-11 09:49:43 +0530

    Time: 1 ms (100.00%), Space: 40.2 MB (34.16%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,18 +1,18 @@
 class Solution {
     public int arrangeCoins(int n) {
-        long l = 0,r=n;
-        while(l<=r){
-            long m = l+(r-l)/2;
-            long k = m*(m+1)/2;
-            if(k==n)
-            return (int)m;
-            else if(k>n)
-            {
-                r = m-1;
-            }
-            else
-            l=m+1;
-        }
-        return (int)r;
+        // long l = 0,r=n;
+        // while(l<=r){
+        //     long m = l+(r-l)/2;
+        //     long k = m*(m+1)/2;
+        //     if(k==n)
+        //     return (int)m;
+        //     else if(k>n)
+        //     {
+        //         r = m-1;
+        //     }
+        //     else
+        //     l=m+1;
+        // }
+        return (int)(Math.sqrt(2 * (long)n + 0.25)-0.5);
     }
 }
