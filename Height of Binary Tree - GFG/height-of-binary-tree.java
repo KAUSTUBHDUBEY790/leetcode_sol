5f7d0b90b2af10efd28d425243c68d5c8147a3d2
Kaustubh Dubey <78341839+KAUSTUBHDUBEY790@users.noreply.github.com>
2023-06-06 13:22:24 +0530

    Added solution - LeetHub

--- Height of Binary Tree - GFG/height-of-binary-tree.java
+++ Height of Binary Tree - GFG/height-of-binary-tree.java
@@ -124,11 +124,12 @@ class Solution {
     int height(Node node) 
     {
         // code here 
-        if(node==null)
-        return 0;
-        int r = height(node.left);
-        int l = height(node.right);
-        int h = Math.max(r,l)+1;
-        return h;
+        if(node == null)
+        {
+            return 0;
+        }
+        int lh = height(node.left);
+        int rh = height(node.right);
+        return Math.max(lh,rh)+1;
     }
 }
