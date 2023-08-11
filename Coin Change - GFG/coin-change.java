93fd40cc6120fec0fe748bb7a28be390b12858bb
Kaustubh Dubey <78341839+KAUSTUBHDUBEY790@users.noreply.github.com>
2023-08-11 12:49:20 +0530

    Added solution - LeetHub

---
+++ Coin Change - GFG/coin-change.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int sum = sc.nextInt();
+            int N = sc.nextInt();
+            int coins[] = new int[N];
+            for (int i = 0; i < N; i++) coins[i] = sc.nextInt();
+            Solution ob = new Solution();
+            System.out.println(ob.count(coins, N, sum));
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public long count(int coins[], int N, int sum) {
+        // code here.
+         long[] dp = new long[sum + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= sum; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+
+        return dp[sum];
+    }
+}
