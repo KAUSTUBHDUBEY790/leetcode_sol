e2e3a40468680af11e6d9ec7ed4870dfb99f868b
Kaustubh Dubey <78341839+KAUSTUBHDUBEY790@users.noreply.github.com>
2023-09-01 14:49:35 +0530

    Time: 22 ms (41.49%), Space: 42.4 MB (99.40%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,16 +1,30 @@
-public class Solution {
- public int coinChange(int[] coins, int amount) {
- int max = amount + 1;
- int[] dp = new int[amount + 1];
- Arrays.fill(dp, max);
- dp[0] = 0;
- for (int i = 1; i <= amount; i++) {
- for (int j = 0; j < coins.length; j++) {
- if (coins[j] <= i) {
- dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
- }
- }
- }
- return dp[amount] > amount ? -1 : dp[amount];
- }
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int dp[] = new int[amount+1];
+        Arrays.fill(dp,-1);
+        dp[0]=0;
+        return minCoins(amount,coins,dp)>amount?-1:minCoins(amount,coins,dp);
+    }
+    public int minCoins(int n, int a[], int dp[]) {
+		
+		if(n == 0) return 0;
+		
+		int ans = Integer.MAX_VALUE;
+		
+		for(int i = 0; i<a.length; i++) {
+			if(n-a[i] >= 0) {
+				int subAns = 0;
+				if(dp[n-a[i]] != -1) {
+					subAns = dp[n-a[i]];
+				} else {
+					subAns = minCoins(n-a[i], a, dp);
+				}
+				if(subAns != Integer.MAX_VALUE && 
+						subAns + 1 < ans) {
+					ans = subAns + 1;
+				}
+			}
+		}
+		return dp[n] = ans;
+	}
 }
