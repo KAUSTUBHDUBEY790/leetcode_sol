d85afb3da5332847c527e38c584d055bf7d871a7
Kaustubh Dubey <78341839+KAUSTUBHDUBEY790@users.noreply.github.com>
2023-09-28 19:58:31 +0530

    Added solution - LeetHub

---
+++ Count pairs with given sum - GFG/count-pairs-with-given-sum.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int k = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            int ans = new Solution().getPairsCount(arr, n, k);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int getPairsCount(int[] arr, int n, int k) {
+        // code here
+        HashMap<Integer,Integer> m=new HashMap<>();
+        int s=0;
+        for(int i=0;i<n;i++){
+            int a=k-arr[i];
+            if(m.containsKey(a))
+            s+=m.get(a);
+            m.put(arr[i],m.getOrDefault(arr[i],0)+1);
+            
+        }
+        return s;
+    }
+}
