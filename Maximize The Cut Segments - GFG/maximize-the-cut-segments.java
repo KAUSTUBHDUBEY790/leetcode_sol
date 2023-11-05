9b32a5329e7aee80d2ca6d2157a6983aebdb6afc
Kaustubh Dubey <78341839+KAUSTUBHDUBEY790@users.noreply.github.com>
2023-11-05 23:52:33 +0530

    Added solution - LeetHub

--- Maximize The Cut Segments - GFG/maximize-the-cut-segments.java
+++ Maximize The Cut Segments - GFG/maximize-the-cut-segments.java
@@ -58,11 +58,11 @@ class Solution
     dp[0] = 0;
     for(int i=1;i<=n;i++)
     {
-        if(i-x>=0)
+        if(i-x>=0 && dp[i-x]!=-1)
         dp[i] = Math.max(dp[i],dp[i-x]+1);
-        if(i-y>=0)
+        if(i-y>=0 && dp[i-y]!=-1)
         dp[i] = Math.max(dp[i],dp[i-y]+1);
-        if(i-z>=0)
+        if(i-z>=0 && dp[i-z]!=-1)
         dp[i] = Math.max(dp[i],dp[i-z]+1);
     }
     return dp[n]<0?0:dp[n];
