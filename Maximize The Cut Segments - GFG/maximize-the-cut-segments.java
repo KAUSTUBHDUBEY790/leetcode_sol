d57bfaa740deb2d066f0111e6860d095c4fb4cb8
Kaustubh Dubey <78341839+KAUSTUBHDUBEY790@users.noreply.github.com>
2023-11-05 23:50:38 +0530

    Added solution - LeetHub

--- Maximize The Cut Segments - GFG/maximize-the-cut-segments.java
+++ Maximize The Cut Segments - GFG/maximize-the-cut-segments.java
@@ -48,11 +48,24 @@ class Solution
     public int maximizeCuts(int n, int x, int y, int z)
     {
        //Your code here
-      int a[] = {x,y,z};
-        int dp[] = new int[n+1];
-        Arrays.fill(dp,-1);
-        int k = seg(n, a,dp);
-        return k<0?0:k;
+    //   int a[] = {x,y,z};
+    //     int dp[] = new int[n+1];
+    //     Arrays.fill(dp,-1);
+    //     int k = seg(n, a,dp);
+    //     return k<0?0:k;
+    int dp[] = new int[n+1];
+    Arrays.fill(dp,Integer.MIN_VALUE);
+    dp[0] = 0;
+    for(int i=1;i<=n;i++)
+    {
+        if(i-x>=0)
+        dp[i] = Math.max(dp[i],dp[i-x]+1);
+        if(i-y>=0)
+        dp[i] = Math.max(dp[i],dp[i-y]+1);
+        if(i-z>=0)
+        dp[i] = Math.max(dp[i],dp[i-z]+1);
+    }
+    return dp[n]<0?0:dp[n];
     }
     public static int seg(int n,int a[],int[] dp)
     {
