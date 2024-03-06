87028c708620d0203ad8887b344746058026fd26
Kaustubh Dubey <78341839+KAUSTUBHDUBEY790@users.noreply.github.com>
2024-03-07 00:10:03 +0530

    Time: 1 ms (93.65%), Space: 45.8 MB (46.29%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int k=0;
+        for(int i=1;i<prices.length;i++)
+            if(prices[i]>prices[i-1])
+                k+=prices[i]-prices[i-1];
+        return k;
+        
+    }
+}
