33eec7330f58199ed40262c3a10d69671d5d7edf
Kaustubh Dubey <78341839+KAUSTUBHDUBEY790@users.noreply.github.com>
2024-03-15 12:17:25 +0530

    Time: 2 ms (73.94%), Space: 55.2 MB (6.11%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,29 +1,29 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int k=1;
-        int z = 0;
+        int a = 1;
+        int z=0;
+        for(int i:nums)
+        {
+            if(i==0)
+            {
+                z++;
+                continue;
+            }
+            a*=i;
+        }
+        if(z>1)
+            Arrays.fill(nums,0);
+        else{
         for(int i=0;i<nums.length;i++)
         {
             if(nums[i]==0)
-            {
-                z++;
-                continue;}
-            k*=nums[i];
-        }
-    if(z==nums.length)
-    {Arrays.fill(nums,0);
-     return nums;
-    }
-     for(int i=0;i<nums.length;i++)
-     {if(z>0)
-     {
-         if(nums[i]==0 && z==1)
-             nums[i] = k;
-         else
-             nums[i] = 0;
-     }
-        else
-         nums[i] = k/nums[i];}
-    return nums;
+                nums[i]= a;
+            else if(z==1)
+                nums[i]=0;
+            else
+                nums[i] = a/nums[i];
+        }}
+        return nums;
+        
     }
 }
