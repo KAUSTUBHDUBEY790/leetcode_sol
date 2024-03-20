664d346146ea1ab85924e59361bc44ca69980cc8
Kaustubh Dubey <78341839+KAUSTUBHDUBEY790@users.noreply.github.com>
2024-03-20 16:41:00 +0530

    Time: 2 ms (40.65%), Space: 46.3 MB (80.56%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.java
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.java
@@ -13,22 +13,34 @@ class Solution {
         ListNode k = new ListNode(0);
         ListNode l = new ListNode(0);
         ListNode n = new ListNode(0);
+        ListNode h2 = list2;
+        while(h2.next!=null)
+            h2 = h2.next;
         n.next = list1;
         ListNode h = list1;
         for(int i=0;i<=b;i++)
         {
             if(i==a)
-                k = n;
+            {n.next = list2;
+             while(i<=b)
+             {
+                 if(i==b)
+                     h2.next = h.next;
+                 h = h.next;
+                 i++;
+             }
+             break;
+            }
             if(i==b)
                 l = h.next;
             n = n.next;
             h = h.next;    
         }
-        k.next = list2;
-        h = list1;
-        while(h.next!=null)
-            h = h.next;
-        h.next = l;
+        // k.next = list2;
+        // h = list1;
+        // while(h.next!=null)
+        //     h = h.next;
+        // h.next = l;
         return list1;
         
     }
