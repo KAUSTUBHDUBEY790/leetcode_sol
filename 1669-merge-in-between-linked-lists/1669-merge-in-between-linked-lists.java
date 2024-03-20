133f5d4b801e454f6deeed96a7cb57686fdd17c2
Kaustubh Dubey <78341839+KAUSTUBHDUBEY790@users.noreply.github.com>
2024-03-20 16:42:08 +0530

    Time: 2 ms (40.65%), Space: 46.9 MB (15.23%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.java
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.java
@@ -10,38 +10,27 @@
  */
 class Solution {
     public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
-        ListNode k = new ListNode(0);
-        ListNode l = new ListNode(0);
-        ListNode n = new ListNode(0);
-        ListNode h2 = list2;
-        while(h2.next!=null)
-            h2 = h2.next;
-        n.next = list1;
-        ListNode h = list1;
-        for(int i=0;i<=b;i++)
-        {
-            if(i==a)
-            {n.next = list2;
-             while(i<=b)
-             {
-                 if(i==b)
-                     h2.next = h.next;
-                 h = h.next;
-                 i++;
-             }
-             break;
+        ListNode start = null;
+        ListNode end = list1;
+
+        // Set start to node a - 1 and end to node b
+        for (int index = 0; index < b; index++) {
+            if (index == a - 1) {
+                start = end;
             }
-            if(i==b)
-                l = h.next;
-            n = n.next;
-            h = h.next;    
+            end = end.next;
         }
-        // k.next = list2;
-        // h = list1;
-        // while(h.next!=null)
-        //     h = h.next;
-        // h.next = l;
-        return list1;
+        // Connect the start node to list2
+        start.next = list2;
+
+        // Find the tail of list2
+        while (list2.next != null) {
+            list2 = list2.next;
+        }
+        // Set the tail of list2 to end.next
+        list2.next = end.next;
+        end.next = null;
         
+        return list1;
     }
 }
