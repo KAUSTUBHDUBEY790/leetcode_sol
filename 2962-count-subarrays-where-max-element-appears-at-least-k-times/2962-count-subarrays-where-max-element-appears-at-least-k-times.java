3ab40009a35f0d1c0441924c4160f3a88a347542
Kaustubh Dubey <78341839+KAUSTUBHDUBEY790@users.noreply.github.com>
2024-03-29 11:50:33 +0530

    Time: 5 ms (84.18%), Space: 65.7 MB (34.18%) - LeetHub

---
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.java
@@ -0,0 +1,27 @@
+class Solution {
+    public long countSubarrays(int[] nums, int k) {
+        int l = nums[0];
+        for(int i:nums)
+            l = Math.max(l,i);
+        int i=0;
+        long ans =0;
+        int g=0;
+        for(int j=0;j<nums.length;j++)
+        {
+            if(nums[j]==l)
+                g++;
+            while(g==k)
+            {
+                // ans++;
+                if(nums[i]==l)
+                {
+                 g--;   
+                }
+                i++;
+            }
+            ans+=i;
+                 
+       }
+        return ans;
+    }
+}
