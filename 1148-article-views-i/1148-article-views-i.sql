d88d97bc0cb13518842415277b7961a2bdf40d8f
Kaustubh Dubey <78341839+KAUSTUBHDUBEY790@users.noreply.github.com>
2024-06-04 00:38:43 +0530

    Time: 873 ms (41.46%), Space: 0B (100.00%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,6 +1,2 @@
 # Write your MySQL query statement below
-select author_id as id
-from Views
-where author_id like viewer_id
-group by author_id
-order by author_id
+select distinct author_id as id from views where author_id=viewer_id order by id asc;
